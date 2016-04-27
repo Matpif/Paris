918d8e10625d063b15eb0551a013aa4ec129c257
matpif <matthieu.piffeteau@gmail.com>
2016-04-27 20:42:02 +0200

    Add sheetstyle and edit message

--- app/view/page.php
+++ app/view/page.php
@@ -42,13 +42,14 @@ $messages = $messageManager->getMessages();
         <title><?php echo $_controller->getTitle(); ?></title>
         <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
         <script src="js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
     <body>
         <?php $_controller->getHeader(); ?>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 col-lg-offset-6">
+                <div class="col-lg-4 col-lg-offset-4">
                     <?php foreach ($messages as $message): ?>
                         <?php echo $message->getMessageHtml(); ?><br/>
                     <?php endforeach; ?>
