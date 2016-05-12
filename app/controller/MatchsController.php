2f9af66f776811482c4fcc88ba6ab064a51f7420
matpif <matthieu.piffeteau@gmail.com>
2016-05-12 13:47:02 +0200

    Delete test method

--- app/controller/MatchsController.php
+++ app/controller/MatchsController.php
@@ -22,22 +22,6 @@ class MatchsController extends Controller
         $this->_page = 'Matchs';
     }
 
-    public function testAction () {
-        $collection = new MatchCollection();
-        $collection->loadAll('date');
-
-        /** @var MatchModel $match */
-        foreach ($collection as $match) {
-            $equipe_1 = (new EquipeCollection())->loadById($match->getAttribute('equipe_id_1'));
-            $equipe_2 = (new EquipeCollection())->loadById($match->getAttribute('equipe_id_2'));
-
-            echo 'Date : ' . $match->getAttribute('date') . '<br/>';
-            echo '<img width="35px" src="/images/pays/' . $equipe_1->getAttribute('image') . '" />' . $equipe_1->getAttribute('name') . ' - ' . $equipe_2->getAttribute('name') . '<img width="35px" src="/images/pays/' . $equipe_2->getAttribute('image') . '" />';
-            echo '<br/><br/>';
-        }
-        die;
-    }
-
     /**
      * @return MatchCollection
      */
@@ -52,6 +36,7 @@ class MatchsController extends Controller
      */
     public function getPari($match) {
 
+        /** @var UtilisateurModel $_utilisateur */
         $_utilisateur = Access::getInstance()->getCurrentUser();
         if ($this->_pariCollection == null) {
             $this->_pariCollection = new PariCollection();
