3e03a5095a75cecacb152978b2ac51c25f809e83
CorentinPasquier <co-pasquier@orange.fr>
2016-05-16 22:15:59 +0200

    getScore function
    implement getScore function

--- app/model/MatchModel.php
+++ app/model/MatchModel.php
@@ -30,4 +30,31 @@ class MatchModel extends Model
         $equipeCollection = new EquipeCollection();
         return $equipeCollection->loadById($this->getAttribute('equipe_id_2'));
     }
+    
+    /**
+     * Get score by team name key
+     * @param $key int
+     * @return mixed|string
+     */
+    public function getScore($key) {
+        if($key == 1){
+            if (is_array($this->_data) && array_key_exists('score_equipe_1', $this->_data)){
+                return $this->_data['score_equipe_1'];
+            }
+            else{
+                return '-';
+            }
+        }
+        elseif($key == 2){
+            if (is_array($this->_data) && array_key_exists('score_equipe_2', $this->_data)){
+                return $this->_data['score_equipe_2'];
+            }
+            else{
+                return '-';
+            }
+        }
+        else{
+            return '-';
+        }
+    }
 }
