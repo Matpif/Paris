1d148612c042175343ea00cb8ec6351fcac44cbf
matpif <matthieu.piffeteau@gmail.com>
2016-06-01 21:35:32 +0200

    Add google analytics

--- app/view/page.php
+++ app/view/page.php
@@ -65,6 +65,19 @@ $messages = $messageManager->getMessages();
         <?php endforeach; ?>
     </head>
     <body>
+        <?php
+        if (ReadIni::getInstance()->getAttribute('general', 'google_analytics')): ?>
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', '<?php echo ReadIni::getInstance()->getAttribute('general', 'google_analytics'); ?>', 'auto');
+                ga('send', 'pageview');
+
+            </script>
+        <?php endif; ?>
         <?php echo $_controller->getHeader(); ?>
         <div class="container-fluid body">
             <div class="row">
